41d952def7e9e88f8912c4516d8ef52f4d3fbf99
Byron Ochara <byron.ochara@gmail.com>
2026-02-10 23:25:55 +0300

    updated CORS for deployment

--- backend/app.js
+++ backend/app.js
@@ -14,25 +14,24 @@ const app = express();
 
 app.use(express.json({ limit: '10kb' }));
 
-// const allowedOrigins = ['https://nissmart-engine-ledger.vercel.app', 'http://localhost:4005'];
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   }
-// }));
-
-const corsOptions = {
-  origin: 'https://nissmart-engine-ledger.vercel.app', // Replace with your actual Vercel frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-  credentials: false // Allow cookies/authorization headers to be sent
-};
-
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'localhost:3000'); // update to match the domain you will make the request from
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization'
+  );
+  res.setHeader(
+    'Report-To',
+    '{"group":"csp-endpoint","max_age":10886400,"endpoints":[{"url":"http://localhost:4005/__cspreport__"}],"include_subdomains":true}'
+  );
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; font-src 'self' https:; img-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; frame-src 'self'; style-src-elem 'self' 'unsafe-inline' https:;"
+  );
+  next();
+});
 
 
 
